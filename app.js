99002fb4d344c69f7deecefe654dc8d35d417a9c
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-06-02 16:07:16 +0200

    array, tuple, enum

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
-//                object, array
-// console.log(person.nickname); throw a error because nickname is not a property of person
+//                object, Array, Tuple
+/*
 // object types explicit
-var person2 = {
-    name: "Andrea",
-    age: 28,
+const person2: object = {
+  name: "Andrea",
+  age: 28,
 };
+
 // object types explicit with properties
-var person3 = {
-    name: "Andrea",
-    age: 28,
+const person3: {
+  name: string;
+  age: number;
+} = {
+  name: "Andrea",
+  age: 28,
 };
+
 // object types - Thi is the best syntax to use
+const person = {
+  name: "Andrea",
+  age: 28,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+// console.log(person.nickname); throw a error because nickname is not a property of person
+
+// you can push a new value to the tuple, but not assign a complete new tuple to the tuple, because the tuple has a fixed length (except if you use push)
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+*/
+//                Enum
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Andrea",
     age: 28,
     hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN,
 };
-var favoriteActivities;
-favoriteActivities = ["Sports"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-//                object, Array
-
+//                object, Array, Tuple
+/*
 // object types explicit
 const person2: object = {
   name: "Andrea",
@@ -20,9 +20,12 @@ const person = {
   name: "Andrea",
   age: 28,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
 // console.log(person.nickname); throw a error because nickname is not a property of person
+
+// you can push a new value to the tuple, but not assign a complete new tuple to the tuple, because the tuple has a fixed length (except if you use push)
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -31,4 +34,36 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+*/
+
+//                Enum
+
+enum Role {
+  ADMIN, // 0
+  READ_ONLY, // 1
+  AUTHOR, // 2
+}
+
+// you can assign a custom value to the enum
+enum Role2 {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+enum Role3 {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = "AUTHOR",
+}
+
+const person = {
+  name: "Andrea",
+  age: 28,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
