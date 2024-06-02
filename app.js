ebd5c22243e9f899968106fad328fe453bd4aa53
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-06-02 23:06:38 +0200

    type alias

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-//                Union Types
-function combine(input1, input2, resultConversion) {
+//                Union Types, Literal Types, Type Aliases
+function combine(input1, // Union Types
+input2, // Type Alias
+resultConversion // Literal Types
+) {
     var result;
     if ((typeof input1 === "number" && typeof input2 === "number") ||
         resultConversion === "as-number") {
@@ -12,8 +15,21 @@ function combine(input1, input2, resultConversion) {
 }
 var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedStringAges = combine(30, 26, "as-text");
+var combinedStringAges = combine(30, 26, "as-number");
 console.log(combinedStringAges);
 var combinedNames = combine("Andrea", "Giulia", "as-text");
 console.log(combinedNames);
-//                Literal Types
+//  Type Alias Example
+// from this
+function greet(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
+function greetWithAlias(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlderWithAlias(user, checkAge) {
+    return checkAge > user.age;
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-//                Union Types, Literal Types
+//                Union Types, Literal Types, Type Aliases
+
+type Combinable = number | string; // Type Alias
+type ConversionDescriptor = "as-number" | "as-text"; // Literal Types
 
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: number | string, // Union Types
+  input2: Combinable, // Type Alias
+  resultConversion: ConversionDescriptor // Literal Types
 ) {
   let result;
   if (
@@ -25,3 +28,24 @@ console.log(combinedStringAges);
 
 const combinedNames = combine("Andrea", "Giulia", "as-text");
 console.log(combinedNames);
+
+//  Type Alias Example
+// from this
+function greet(user: { name: string; age: number }) {
+  console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: { name: string; age: number }, checkAge: number) {
+  return checkAge > user.age;
+}
+
+// to this
+type User = { name: string; age: number };
+
+function greetWithAlias(user: User) {
+  console.log("Hi, I am " + user.name);
+}
+
+function isOlderWithAlias(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
