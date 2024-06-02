4c4d7f4c198b0c089eea9761e37caf32794a6135
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-06-02 22:51:46 +0200

    union types & literal types

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-function combine(input1, input2) {
+//                Union Types
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Andrea", "Giulia");
+var combinedStringAges = combine(30, 26, "as-text");
+console.log(combinedStringAges);
+var combinedNames = combine("Andrea", "Giulia", "as-text");
 console.log(combinedNames);
+//                Literal Types
